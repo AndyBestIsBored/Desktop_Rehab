97b870955811735f11c0cd9ab8f52b91248fcac5
JoeyPK <127579632+JoeyPK@users.noreply.github.com>
2023-08-11 15:53:46 +0700

    matlab

--- MATLAB_code/Analyze.m
+++ MATLAB_code/Analyze.m
@@ -7,6 +7,7 @@ clc
 mainpath = "/Users/khemwutapornpipatsakul/Documents/VS_coding";
 
 %-----Read data-----%
+
 %X25_Y10
 X25_Y10_T02 = process(readtable(mainpath+"/Desktop_Rehab/Record_Data/X025/Y10_T02_Static.csv"));
 X25_Y10_T20 = process(readtable(mainpath+"/Desktop_Rehab/Record_Data/X025/Y10_T20_Static.csv"));
@@ -43,65 +44,102 @@ X25_Y19_Tm20 = process(readtable(mainpath+"/Desktop_Rehab/Record_Data/X025/Y19_T
 X25_Y19_Tm2m2 = process(readtable(mainpath+"/Desktop_Rehab/Record_Data/X025/Y19_T-2-2_Static.csv"));
 
 
+%-----Average data-----%
+
+%X25_Y10
+X25_Y10_T02_avg = avg(X25_Y10_T02);
+X25_Y10_T20_avg = avg(X25_Y10_T20);
+X25_Y10_T22_avg = avg(X25_Y10_T22);
+X25_Y10_T0m2_avg = avg(X25_Y10_T0m2);
+X25_Y10_Tm20_avg = avg(X25_Y10_Tm20);
+X25_Y10_Tm2m2_avg = avg(X25_Y10_Tm2m2);
+
+
+%X25_Y13
+X25_Y13_T02_avg = avg(X25_Y13_T02);
+X25_Y13_T20_avg = avg(X25_Y13_T20);
+X25_Y13_T22_avg = avg(X25_Y13_T22);
+X25_Y13_T0m2_avg = avg(X25_Y13_T0m2);
+X25_Y13_Tm20_avg = avg(X25_Y13_Tm20);
+X25_Y13_Tm2m2_avg = avg(X25_Y13_Tm2m2);
+
+
+%X25_Y16
+X25_Y16_T02_avg = avg(X25_Y16_T02);
+X25_Y16_T20_avg = avg(X25_Y16_T20);
+X25_Y16_T22_avg = avg(X25_Y16_T22);
+X25_Y16_T0m2_avg = avg(X25_Y16_T0m2);
+X25_Y16_Tm20_avg = avg(X25_Y16_Tm20);
+X25_Y16_Tm2m2_avg = avg(X25_Y16_Tm2m2);
+
+
+%X25_Y19
+X25_Y19_T02_avg = avg(X25_Y19_T02);
+X25_Y19_T20_avg = avg(X25_Y19_T20);
+X25_Y19_T22_avg = avg(X25_Y19_T22);
+X25_Y19_T0m2_avg = avg(X25_Y19_T0m2);
+X25_Y19_Tm20_avg = avg(X25_Y19_Tm20);
+X25_Y19_Tm2m2_avg = avg(X25_Y19_Tm2m2);
+
 %-----Stack-----%
 %T20
-X25_T20_Fx = [X25_Y10_T20.Fx,X25_Y13_T20.Fx,X25_Y16_T20.Fx,X25_Y19_T20.Fx];
-X25_T20_Fy = [X25_Y10_T20.Fy,X25_Y13_T20.Fy,X25_Y16_T20.Fy,X25_Y19_T20.Fy];
-X25_T20_Fx_Mea = [X25_Y10_T20.Fx_Mea,X25_Y13_T20.Fx_Mea,X25_Y16_T20.Fx_Mea,X25_Y19_T20.Fx_Mea];
-X25_T20_Fy_Mea = [X25_Y10_T20.Fy_Mea,X25_Y13_T20.Fy_Mea,X25_Y16_T20.Fy_Mea,X25_Y19_T20.Fy_Mea];
+X25_T20_Fx_avg = [X25_Y10_T20_avg.Fx,X25_Y13_T20_avg.Fx,X25_Y16_T20_avg.Fx,X25_Y19_T20_avg.Fx];
+X25_T20_Fy_avg = [X25_Y10_T20_avg.Fy,X25_Y13_T20_avg.Fy,X25_Y16_T20_avg.Fy,X25_Y19_T20_avg.Fy];
+X25_T20_Fx_Mea_avg = [X25_Y10_T20_avg.Fx_Mea,X25_Y13_T20_avg.Fx_Mea,X25_Y16_T20_avg.Fx_Mea,X25_Y19_T20_avg.Fx_Mea];
+X25_T20_Fy_Mea_avg = [X25_Y10_T20_avg.Fy_Mea,X25_Y13_T20_avg.Fy_Mea,X25_Y16_T20_avg.Fy_Mea,X25_Y19_T20_avg.Fy_Mea];
 
 %T02
-X25_T02_Fx = [X25_Y10_T02.Fx,X25_Y13_T02.Fx,X25_Y16_T02.Fx,X25_Y19_T02.Fx];
-X25_T02_Fy = [X25_Y10_T02.Fy,X25_Y13_T02.Fy,X25_Y16_T02.Fy,X25_Y19_T02.Fy];
-X25_T02_Fx_Mea = [X25_Y10_T02.Fx_Mea,X25_Y13_T02.Fx_Mea,X25_Y16_T02.Fx_Mea,X25_Y19_T02.Fx_Mea];
-X25_T02_Fy_Mea = [X25_Y10_T02.Fy_Mea,X25_Y13_T02.Fy_Mea,X25_Y16_T02.Fy_Mea,X25_Y19_T02.Fy_Mea];
+X25_T02_Fx_avg = [X25_Y10_T02_avg.Fx,X25_Y13_T02_avg.Fx,X25_Y16_T02_avg.Fx,X25_Y19_T02_avg.Fx];
+X25_T02_Fy_avg = [X25_Y10_T02_avg.Fy,X25_Y13_T02_avg.Fy,X25_Y16_T02_avg.Fy,X25_Y19_T02_avg.Fy];
+X25_T02_Fx_Mea_avg = [X25_Y10_T02_avg.Fx_Mea,X25_Y13_T02_avg.Fx_Mea,X25_Y16_T02_avg.Fx_Mea,X25_Y19_T02_avg.Fx_Mea];
+X25_T02_Fy_Mea_avg = [X25_Y10_T02_avg.Fy_Mea,X25_Y13_T02_avg.Fy_Mea,X25_Y16_T02_avg.Fy_Mea,X25_Y19_T02_avg.Fy_Mea];
 
 %T22
-X25_T22_Fx = [X25_Y10_T22.Fx,X25_Y13_T22.Fx,X25_Y16_T22.Fx,X25_Y19_T22.Fx];
-X25_T22_Fy = [X25_Y10_T22.Fy,X25_Y13_T22.Fy,X25_Y16_T22.Fy,X25_Y19_T22.Fy];
-X25_T22_Fx_Mea = [X25_Y10_T22.Fx_Mea,X25_Y13_T22.Fx_Mea,X25_Y16_T22.Fx_Mea,X25_Y19_T22.Fx_Mea];
-X25_T22_Fy_Mea = [X25_Y10_T22.Fy_Mea,X25_Y13_T22.Fy_Mea,X25_Y16_T22.Fy_Mea,X25_Y19_T22.Fy_Mea];
+X25_T22_Fx_avg = [X25_Y10_T22_avg.Fx,X25_Y13_T22_avg.Fx,X25_Y16_T22_avg.Fx,X25_Y19_T22_avg.Fx];
+X25_T22_Fy_avg = [X25_Y10_T22_avg.Fy,X25_Y13_T22_avg.Fy,X25_Y16_T22_avg.Fy,X25_Y19_T22_avg.Fy];
+X25_T22_Fx_Mea_avg = [X25_Y10_T22_avg.Fx_Mea,X25_Y13_T22_avg.Fx_Mea,X25_Y16_T22_avg.Fx_Mea,X25_Y19_T22_avg.Fx_Mea];
+X25_T22_Fy_Mea_avg = [X25_Y10_T22_avg.Fy_Mea,X25_Y13_T22_avg.Fy_Mea,X25_Y16_T22_avg.Fy_Mea,X25_Y19_T22_avg.Fy_Mea];
 
 %Tm20
-X25_Tm20_Fx = [X25_Y10_Tm20.Fx,X25_Y13_Tm20.Fx,X25_Y16_Tm20.Fx,X25_Y19_Tm20.Fx];
-X25_Tm20_Fy = [X25_Y10_Tm20.Fy,X25_Y13_Tm20.Fy,X25_Y16_Tm20.Fy,X25_Y19_Tm20.Fy];
-X25_Tm20_Fx_Mea = [X25_Y10_Tm20.Fx_Mea,X25_Y13_Tm20.Fx_Mea,X25_Y16_Tm20.Fx_Mea,X25_Y19_Tm20.Fx_Mea];
-X25_Tm20_Fy_Mea = [X25_Y10_Tm20.Fy_Mea,X25_Y13_Tm20.Fy_Mea,X25_Y16_Tm20.Fy_Mea,X25_Y19_Tm20.Fy_Mea];
+X25_Tm20_Fx_avg = [X25_Y10_Tm20_avg.Fx,X25_Y13_Tm20_avg.Fx,X25_Y16_Tm20_avg.Fx,X25_Y19_Tm20_avg.Fx];
+X25_Tm20_Fy_avg = [X25_Y10_Tm20_avg.Fy,X25_Y13_Tm20_avg.Fy,X25_Y16_Tm20_avg.Fy,X25_Y19_Tm20_avg.Fy];
+X25_Tm20_Fx_Mea_avg = [X25_Y10_Tm20_avg.Fx_Mea,X25_Y13_Tm20_avg.Fx_Mea,X25_Y16_Tm20_avg.Fx_Mea,X25_Y19_Tm20_avg.Fx_Mea];
+X25_Tm20_Fy_Mea_avg = [X25_Y10_Tm20_avg.Fy_Mea,X25_Y13_Tm20_avg.Fy_Mea,X25_Y16_Tm20_avg.Fy_Mea,X25_Y19_Tm20_avg.Fy_Mea];
 
 %T0m2
-X25_T0m2_Fx = [X25_Y10_T0m2.Fx,X25_Y13_T0m2.Fx,X25_Y16_T0m2.Fx,X25_Y19_T0m2.Fx];
-X25_T0m2_Fy = [X25_Y10_T0m2.Fy,X25_Y13_T0m2.Fy,X25_Y16_T0m2.Fy,X25_Y19_T0m2.Fy];
-X25_T0m2_Fx_Mea = [X25_Y10_T0m2.Fx_Mea,X25_Y13_T0m2.Fx_Mea,X25_Y16_T0m2.Fx_Mea,X25_Y19_T0m2.Fx_Mea];
-X25_T0m2_Fy_Mea = [X25_Y10_T0m2.Fy_Mea,X25_Y13_T0m2.Fy_Mea,X25_Y16_T0m2.Fy_Mea,X25_Y19_T0m2.Fy_Mea];
+X25_T0m2_Fx_avg = [X25_Y10_T0m2_avg.Fx,X25_Y13_T0m2_avg.Fx,X25_Y16_T0m2_avg.Fx,X25_Y19_T0m2_avg.Fx];
+X25_T0m2_Fy_avg = [X25_Y10_T0m2_avg.Fy,X25_Y13_T0m2_avg.Fy,X25_Y16_T0m2_avg.Fy,X25_Y19_T0m2_avg.Fy];
+X25_T0m2_Fx_Mea_avg = [X25_Y10_T0m2_avg.Fx_Mea,X25_Y13_T0m2_avg.Fx_Mea,X25_Y16_T0m2_avg.Fx_Mea,X25_Y19_T0m2_avg.Fx_Mea];
+X25_T0m2_Fy_Mea_avg = [X25_Y10_T0m2_avg.Fy_Mea,X25_Y13_T0m2_avg.Fy_Mea,X25_Y16_T0m2_avg.Fy_Mea,X25_Y19_T0m2_avg.Fy_Mea];
 
 %Tm2m2
-X25_Tm2m2_Fx = [X25_Y10_Tm2m2.Fx,X25_Y13_Tm2m2.Fx,X25_Y16_Tm2m2.Fx,X25_Y19_Tm2m2.Fx];
-X25_Tm2m2_Fy = [X25_Y10_Tm2m2.Fy,X25_Y13_Tm2m2.Fy,X25_Y16_Tm2m2.Fy,X25_Y19_Tm2m2.Fy];
-X25_Tm2m2_Fx_Mea = [X25_Y10_Tm2m2.Fx_Mea,X25_Y13_Tm2m2.Fx_Mea,X25_Y16_Tm2m2.Fx_Mea,X25_Y19_Tm2m2.Fx_Mea];
-X25_Tm2m2_Fy_Mea = [X25_Y10_Tm2m2.Fy_Mea,X25_Y13_Tm2m2.Fy_Mea,X25_Y16_Tm2m2.Fy_Mea,X25_Y19_Tm2m2.Fy_Mea];
+X25_Tm2m2_Fx_avg = [X25_Y10_Tm2m2_avg.Fx,X25_Y13_Tm2m2_avg.Fx,X25_Y16_Tm2m2_avg.Fx,X25_Y19_Tm2m2_avg.Fx];
+X25_Tm2m2_Fy_avg = [X25_Y10_Tm2m2_avg.Fy,X25_Y13_Tm2m2_avg.Fy,X25_Y16_Tm2m2_avg.Fy,X25_Y19_Tm2m2_avg.Fy];
+X25_Tm2m2_Fx_Mea_avg = [X25_Y10_Tm2m2_avg.Fx_Mea,X25_Y13_Tm2m2_avg.Fx_Mea,X25_Y16_Tm2m2_avg.Fx_Mea,X25_Y19_Tm2m2_avg.Fx_Mea];
+X25_Tm2m2_Fy_Mea_avg = [X25_Y10_Tm2m2_avg.Fy_Mea,X25_Y13_Tm2m2_avg.Fy_Mea,X25_Y16_Tm2m2_avg.Fy_Mea,X25_Y19_Tm2m2_avg.Fy_Mea];
 
 
 %%Stack
-X25_Fx_Stack = [X25_T20_Fx;X25_T02_Fx;X25_T22_Fx;X25_Tm20_Fx;X25_T0m2_Fx;X25_Tm2m2_Fx];
-X25_Fy_Stack = [X25_T20_Fy;X25_T02_Fy;X25_T22_Fy;X25_Tm20_Fy;X25_T0m2_Fy;X25_Tm2m2_Fy];
-X25_Fx_Mea_Stack = [X25_T20_Fx_Mea;X25_T02_Fx_Mea;X25_T22_Fx_Mea;X25_Tm20_Fx_Mea;X25_T0m2_Fx_Mea;X25_Tm2m2_Fx_Mea];
-X25_Fy_Mea_Stack = [X25_T20_Fy_Mea;X25_T02_Fy_Mea;X25_T22_Fy_Mea;X25_Tm20_Fy_Mea;X25_T0m2_Fy_Mea;X25_Tm2m2_Fy_Mea];
+X25_Fx_Stack_avg = [X25_T20_Fx_avg; X25_T02_Fx_avg; X25_T22_Fx_avg; X25_Tm20_Fx_avg; X25_T0m2_Fx_avg; X25_Tm2m2_Fx_avg];
+X25_Fy_Stack_avg = [X25_T20_Fy_avg; X25_T02_Fy_avg; X25_T22_Fy_avg; X25_Tm20_Fy_avg; X25_T0m2_Fy_avg; X25_Tm2m2_Fy_avg];
+X25_Fx_Mea_Stack_avg = [X25_T20_Fx_Mea_avg; X25_T02_Fx_Mea_avg; X25_T22_Fx_Mea_avg; X25_Tm20_Fx_Mea_avg; X25_T0m2_Fx_Mea_avg; X25_Tm2m2_Fx_Mea_avg];
+X25_Fy_Mea_Stack_avg = [X25_T20_Fy_Mea_avg; X25_T02_Fy_Mea_avg; X25_T22_Fy_Mea_avg; X25_Tm20_Fy_Mea_avg; X25_T0m2_Fy_Mea_avg; X25_Tm2m2_Fy_Mea_avg];
 
 
 
 %-----Plot-----%
 
-X25_Y = [10,13,16,19];
+X25_Y_avg = [10,13,16,19];
 T = ["T20","T02","T22","Tm20","T0m2","Ym2m2"];
 
 %%Fx
 figure()
 sgtitle("Fx X25")
-for i = 1:length(X25_Fx_Stack)
+for i = 1:length(X25_Fx_Stack_avg)
     subplot(2,3,i)
     hold on
-    plot(X25_Y,X25_Fx_Stack(i,:))
-    plot(X25_Y,X25_Fx_Mea_Stack(i,:))
+    plot(X25_Y_avg,X25_Fx_Stack_avg(i,:))
+    plot(X25_Y_avg,X25_Fx_Mea_Stack_avg(i,:))
     hold off
     legend(["Theory","Experiment"])
     title(T(i))
@@ -113,11 +151,11 @@ end
 %%Fy
 figure()
 sgtitle("Fy X25")
-for i = 1:length(X25_Fy_Stack)
+for i = 1:length(X25_Fy_Stack_avg)
     subplot(2,3,i)
     hold on
-    plot(X25_Y,X25_Fy_Stack(i,:))
-    plot(X25_Y,X25_Fy_Mea_Stack(i,:))
+    plot(X25_Y_avg,X25_Fy_Stack_avg(i,:))
+    plot(X25_Y_avg,X25_Fy_Mea_Stack_avg(i,:))
     hold off
     legend(["Theory","Experiment"])
     title(T(i))
@@ -747,7 +785,12 @@ function y = process(x)
     x.Fx_Mea = Fx_real;
     x.Fy_Mea = Fy_real;
 
-    x = x(10:end-10,3:end);
+    y = x(10:end-10,3:end);
+
+end
+
+function y = avg(x)
+
     xx = mean(x{:,:});
     COLUMNS = ["T1", "T2", "t1", "t2", "q1", "q2", "x", "y", "x_dot", "y_dot", "Fx", "Fy", "Fx_Mea", "Fy_Mea"];
     y = array2table(xx,'VariableNames',COLUMNS);
